421bd25ac32adae75746296d73f38b27447845f0
Marat <achkych@gmail.com>
2024-03-17 11:32:00 +0400

    Dzoo5jsp01

---
+++ data.js
@@ -0,0 +1,33 @@
+﻿
+export const products = [
+   {
+      imgProduct: "img/t-shirt.jpg",
+      nameProduct: "Футболка",
+      descriptionProduct: "Очень красивая и удобная футболка в различных цветах.",
+      priceProduct: "25 евро"
+   },
+   {
+      imgProduct: "img/jeans.jpg",
+      nameProduct: "Джинсы",
+      descriptionProduct: "Повседневные джинсы в различных размерах и цветах.",
+      priceProduct: "50 евро"
+   },
+   {
+      imgProduct: "img/sneakers.jpg",
+      nameProduct: "Кроссовки",
+      descriptionProduct: "Удобные кроссовки для спорта и повседневной носки.",
+      priceProduct: "70 евро"
+   },
+   {
+      imgProduct: "img/dress.jpg",
+      nameProduct: "Платье",
+      descriptionProduct: "Элегантное платье с разнообразными деталями.",
+      priceProduct: "40 евро"
+   },
+   {
+      imgProduct: "img/boots.jpg",
+      nameProduct: "Ботинки",
+      descriptionProduct: "Кожаные ботинки на платформе с меховой отделкой.",
+      priceProduct: "80 евро"
+   }
+];
---
+++ script0.js
@@ -0,0 +1,31 @@
+import { products } from "./data.js";
+
+const content = document.querySelector('.content');
+
+products.forEach(({ imgProduct, nameProduct, descriptionProduct, priceProduct }) => {
+   const divProduct = document.createElement('div');
+   divProduct.classList.add('product');
+   content.appendChild(divProduct);
+
+   const img = document.createElement('img');
+   img.classList.add('product__img');
+   img.src = imgProduct;
+   img.width = 200;
+   divProduct.appendChild(img);
+
+   const divProductWrap = document.createElement('div');
+   divProductWrap.classList.add('product__wrap');
+   divProduct.appendChild(divProductWrap);
+
+   const productName = document.createElement('h2');
+   productName.textContent = nameProduct;
+   divProductWrap.appendChild(productName);
+
+   const productDescription = document.createElement('p');
+   productDescription.textContent = descriptionProduct;
+   divProductWrap.appendChild(productDescription);
+
+   const productPrice = document.createElement('p');
+   productPrice.textContent = priceProduct;
+   divProductWrap.appendChild(productPrice);
+});
